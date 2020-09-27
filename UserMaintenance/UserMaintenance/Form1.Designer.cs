c000e40d28c2c40bdaf1d662faf0811f00363250
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-09-27 18:58:51 +0200

    Fullname változások commit

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -29,11 +29,9 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.labelLastname = new System.Windows.Forms.Label();
-            this.labelFirstname = new System.Windows.Forms.Label();
+            this.labelFullname = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -45,23 +43,14 @@
             this.listBox1.Size = new System.Drawing.Size(306, 420);
             this.listBox1.TabIndex = 0;
             // 
-            // labelLastname
+            // labelFullname
             // 
-            this.labelLastname.AutoSize = true;
-            this.labelLastname.Location = new System.Drawing.Point(324, 24);
-            this.labelLastname.Name = "labelLastname";
-            this.labelLastname.Size = new System.Drawing.Size(46, 17);
-            this.labelLastname.TabIndex = 1;
-            this.labelLastname.Text = "label1";
-            // 
-            // labelFirstname
-            // 
-            this.labelFirstname.AutoSize = true;
-            this.labelFirstname.Location = new System.Drawing.Point(324, 67);
-            this.labelFirstname.Name = "labelFirstname";
-            this.labelFirstname.Size = new System.Drawing.Size(46, 17);
-            this.labelFirstname.TabIndex = 2;
-            this.labelFirstname.Text = "label2";
+            this.labelFullname.AutoSize = true;
+            this.labelFullname.Location = new System.Drawing.Point(324, 24);
+            this.labelFullname.Name = "labelFullname";
+            this.labelFullname.Size = new System.Drawing.Size(46, 17);
+            this.labelFullname.TabIndex = 1;
+            this.labelFullname.Text = "label1";
             // 
             // btnAdd
             // 
@@ -80,23 +69,14 @@
             this.textBox1.Size = new System.Drawing.Size(228, 22);
             this.textBox1.TabIndex = 4;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(432, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(228, 22);
-            this.textBox2.TabIndex = 5;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(679, 450);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.labelFirstname);
-            this.Controls.Add(this.labelLastname);
+            this.Controls.Add(this.labelFullname);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -108,11 +88,9 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label labelLastname;
-        private System.Windows.Forms.Label labelFirstname;
+        private System.Windows.Forms.Label labelFullname;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -17,8 +17,8 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            labelLastname.Text = Resource1.LastName;
-            labelFirstname.Text = Resource1.FirstName;
+            labelFullname.Text = Resource1.FullName;
+            
             btnAdd.Text = Resource1.Add;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -29,8 +29,8 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName=textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
         }

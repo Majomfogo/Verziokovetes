06f19b8796912d7c332e49a47dba690bd7d81c0c
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-09-27 18:35:59 +0200

    Funkciók hozzáadása commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             labelLastname.Text = Resource1.LastName;
             labelFirstname.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName=textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }

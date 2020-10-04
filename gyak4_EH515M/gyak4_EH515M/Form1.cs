57304fe03e2af5f9bd567e0170c98b70bf47e170
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-10-04 16:33:32 +0200

    6. feladat commit

--- gyak4_EH515M/gyak4_EH515M/Form1.cs
+++ gyak4_EH515M/gyak4_EH515M/Form1.cs
@@ -12,18 +12,52 @@ using System.Reflection;
 
 namespace gyak4_EH515M
 {
+
+    
     public partial class Form1 : Form
     {
+        Microsoft.Office.Interop.Excel.Application xlApp;
+        Microsoft.Office.Interop.Excel.Workbook xlWB;
+        Microsoft.Office.Interop.Excel.Worksheet xlSheet;
+
         RealEstateEntities context = new RealEstateEntities();
         public List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
+
+
+
         private void LoadData()
         {
             Flats = context.Flat.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Microsoft.Office.Interop.Excel.Application();
+                xlWB = new Microsoft.Office.Interop.Excel.Workbook();
+                xlSheet = new Microsoft.Office.Interop.Excel.Worksheet();
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }                                 
         }
     }
 }

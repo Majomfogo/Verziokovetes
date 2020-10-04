558d82d9b813c97fee5429409f1aaa25ba0bf2e1
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-10-04 17:56:32 +0200

    8. commit

--- gyak4_EH515M/gyak4_EH515M/Form1.cs
+++ gyak4_EH515M/gyak4_EH515M/Form1.cs
@@ -38,26 +38,104 @@ namespace gyak4_EH515M
 
         private void CreateExcel()
         {
-            try
-            {
+        //    try
+        //    {
                 xlApp = new Microsoft.Office.Interop.Excel.Application();
                 xlWB = new Microsoft.Office.Interop.Excel.Workbook();
                 xlSheet = new Microsoft.Office.Interop.Excel.Worksheet();
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
-            }
-            catch (Exception)
+            //}
+            //catch (Exception ex)
+            //{
+            //    string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+            //    MessageBox.Show(errMsg, "Error");
+            //    xlWB.Close(false, Type.Missing, Type.Missing);
+            //    xlApp.Quit();
+            //    xlWB = null;
+            //    xlApp = null;
+            //}                                 
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
             {
-                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
-                MessageBox.Show(errMsg, "Error");
-                xlWB.Close(false, Type.Missing, Type.Missing);
-                xlApp.Quit();
-                xlWB = null;
-                xlApp = null;
-            }                                 
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)" 
+            };
+            for (int i = 1; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1,i] = headers[i-1];
+            }
+
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+
+                if (f.Elevator==true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "="+GetCell(counter+2,8)+"/"+GetCell(counter+2,7);
+
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+            Microsoft.Office.Interop.Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous, Microsoft.Office.Interop.Excel.XlBorderWeight.xlThick);
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }

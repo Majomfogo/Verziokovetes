4d8356b2fe232101ae4ac30aff28438b2b684704
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-11-15 17:54:00 +0100

    7. feladat commit

---
+++ gyak8_EH515M/gyak8_EH515M/Entities/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gyak8_EH515M.Abstractions;
+
+
+
+namespace gyak8_EH515M.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
--- gyak8_EH515M/gyak8_EH515M/Form1.cs
+++ gyak8_EH515M/gyak8_EH515M/Form1.cs
@@ -47,7 +47,7 @@ namespace gyak8_EH515M
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }

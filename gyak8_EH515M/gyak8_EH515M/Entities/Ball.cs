732b0f90b1a554ef13beb3f5320e370973a6b953
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-11-15 17:30:40 +0100

    4. feladat commit

--- gyak8_EH515M/gyak8_EH515M/Entities/Ball.cs
+++ gyak8_EH515M/gyak8_EH515M/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace gyak8_EH515M.Entities
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
             {
--- gyak8_EH515M/gyak8_EH515M/Entities/BallFactory.cs
+++ gyak8_EH515M/gyak8_EH515M/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace gyak8_EH515M.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
